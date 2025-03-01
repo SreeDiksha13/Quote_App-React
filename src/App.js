8700a24d86e7f326aaaae89b7fd450658b9a4be8
SreeDiksha13 <sreediksha45@gmail.com>
2025-03-01 15:20:43 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    state = { quote: '', rotate: false };
+
+    componentDidMount() {
+        this.fetchQuote();
+        window.addEventListener('mousemove', this.handleMouseMove);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('mousemove', this.handleMouseMove);
+    }
+
+    fetchQuote = () => {
+        axios.get('https://api.quotable.io/random')
+            .then((response) => {
+                const quote = response.data.content;
+                this.setState({ quote: quote, rotate: true }, () => {
+                    setTimeout(() => this.setState({ rotate: false }), 1000); 
+                });
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+
+    handleMouseMove = (e) => {
+        const card = document.querySelector('.card');
+        const { offsetWidth, offsetHeight } = card;
+        const x = e.clientX - card.getBoundingClientRect().left - offsetWidth / 2;
+        const y = e.clientY - card.getBoundingClientRect().top - offsetHeight / 2;
+
+        const rotateX = (y / offsetHeight) * 20;
+        const rotateY = -(x / offsetWidth) * 20;
+
+        card.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg) scale(1.05)`;
+    }
+
+    resetTransform = () => {
+        const card = document.querySelector('.card');
+        card.style.transform = 'rotateX(0deg) rotateY(0deg) scale(1)';
+    }
+
+    render() {
+        const { quote, rotate } = this.state;
+        return (
+            <div className="app">
+                <div className="card" onMouseLeave={this.resetTransform}>
+                    <span className={`emoji ${rotate ? 'rotate' : ''}`} role="img" aria-label="sparkle">✨</span>
+                    <h1 className="quote">{quote}</h1>
+                    <button className="button" onClick={this.fetchQuote}>New Quote</button>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
